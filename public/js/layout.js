d4b8e28e032462857872dc1b7876421062db32e7
shiffman <daniel@shiffman.net>
2020-08-12 19:33:56 -0400

    call startCapture when button press

--- public/js/layout.js
+++ public/js/layout.js
@@ -8,17 +8,19 @@ var btn = document.getElementById("submit-video");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
+  // Start video capture when clicking on the button
+  startCapture();
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }

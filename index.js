895e8cac0f00ce2d020574889b560c310a6d77d6
neta <netanyabomani@gmail.com>
2020-08-20 18:44:24 -0400

    netlify functions

--- index.js
+++ index.js
@@ -1,48 +1,52 @@
-const express = require('express');
-const Datastore = require('nedb');
-const multer = require('multer') //use multer to upload blob data
-const upload = multer(); // set multer to be the upload variable (just like express, see above ( include it, then use it/set it up))
-const fs = require('fs'); // use the file system so we can save files
+exports.handler = function (event, context, callback) {
+  
+  const express = require('express');
+  const Datastore = require('nedb');
+  const multer = require('multer') //use multer to upload blob data
+  const upload = multer(); // set multer to be the upload variable (just like express, see above ( include it, then use it/set it up))
+  const fs = require('fs'); // use the file system so we can save files
+  const serverless = require('serverless-http');
+
+  const app = express();
+  app.listen(3000, () => console.log('listening at 3000'));
+  app.use(express.static('public'));
+  app.use(express.json({
+    limit: '10mb'
+  }));
+
+  const database = new Datastore('database.db');
+  database.loadDatabase();
 
 
-const app = express();
-app.listen(3000, () => console.log('listening at 3000'));
-app.use(express.static('public'));
-app.use(express.json({
-  limit: '10mb'
-}));
+  app.post('/upload', upload.single('videoBlob'), function (req, res, next) {
+    let uploadLocation = __dirname + '/public/upload/' + req.file.originalname // where to save the file to. make sure the incoming name has a .wav extension
 
-const database = new Datastore('database.db');
-database.loadDatabase();
-
-
-app.post('/upload', upload.single('videoBlob'), function (req, res, next) {
-  let uploadLocation = __dirname + '/public/upload/' + req.file.originalname // where to save the file to. make sure the incoming name has a .wav extension
-
-  // add the filename 
-  const data = {
-    filename: req.file.originalname,
-    approved: false
-  }
-  database.insert(data);
-
-
-  fs.writeFileSync(uploadLocation, Buffer.from(new Uint8Array(req.file.buffer))); // write the blob to the server as a file
-  res.sendStatus(200); //send back that everything went ok
-  // Another option if you wanted to send a custom message back
-  // res.json({
-  //   status: "it worked!",
-  //   filename: req.file.originalname
-  // });
-})
-
-// connect to ThreeJS - this is a list of webm video uploads
-app.get('/list', (request, response) => {
-  database.find({}, (err, data) => {
-    if (err) {
-      response.end();
-      return;
+    // add the filename 
+    const data = {
+      filename: req.file.originalname,
+      approved: false
     }
-    response.json(data);
+    database.insert(data);
+
+
+    fs.writeFileSync(uploadLocation, Buffer.from(new Uint8Array(req.file.buffer))); // write the blob to the server as a file
+    res.sendStatus(200); //send back that everything went ok
+    // Another option if you wanted to send a custom message back
+    // res.json({
+    //   status: "it worked!",
+    //   filename: req.file.originalname
+    // });
+  })
+
+  // connect to ThreeJS - this is a list of webm video uploads
+  app.get('/list', (request, response) => {
+    database.find({}, (err, data) => {
+      if (err) {
+        response.end();
+        return;
+      }
+      response.json(data);
+    });
   });
-});
+
+}

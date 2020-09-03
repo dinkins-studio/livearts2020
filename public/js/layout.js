e2a4ca6d00651ad8edab3138f4f84d980d77c9a1
neta <netanyabomani@gmail.com>
2020-09-03 18:06:15 -0400

    mute character when video app is open

--- public/js/layout.js
+++ public/js/layout.js
@@ -8,14 +8,29 @@ let modalButton = document.getElementById("submit-video");
 let span = document.getElementsByClassName("close")[0];
 
 modalButton.onclick = function () {
+  // mute depthkit character
+  depthkit.video.muted = true;
+
   modal.style.display = "block";
   // Start video capture when clicking on the button
   startCapture();
 }
 
+// $(modalButton).click(function(event, wasTriggered) {
+//   if (wasTriggered) {
+//     depthkit.video.muted = "muted";
+//       // alert('triggered in code');
+//   } else {
+//       // alert('triggered by mouse');
+//   }
+// });
+
+// $(modalButton).trigger('click', true);
+
 // when a person clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = "none";
+  depthkit.video.muted = false;
 }
 
 window.onclick = function(event) {

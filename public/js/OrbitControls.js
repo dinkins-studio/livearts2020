d57b65e366eeae640cff5c454ae678f9acbbb895
Stephanie Dinkins Studio <dinkinsstudio@gmail.com>
2020-11-17 23:09:05 -0500

    zoom

--- public/js/OrbitControls.js
+++ public/js/OrbitControls.js
@@ -26,8 +26,8 @@ THREE.OrbitControls = function ( object, domElement ) {
         this.target = new THREE.Vector3();
 
         // How far you can dolly in and out ( PerspectiveCamera only )
-        this.minDistance = 1;
-        this.maxDistance = 7;
+        this.minDistance = 3;
+        this.maxDistance = 8;
 
         // How far you can zoom in and out ( OrthographicCamera only )
         this.minZoom = 0;
